c0e7483d5fb490602cdce73f92f674f30df60339
AdrianRos02 <77491499+AdrianRos02@users.noreply.github.com>
2021-06-09 11:38:08 +0200

    Add files via upload

--- RPN.java
+++ RPN.java
@@ -1,11 +1,20 @@
 package calculadora;
 
 public class RPN {
-	public void pushPila(double nuevo_dato) {
+	
+	private String commando;
+	private NodoPila arriba;
+	
+	
+	
+	public void NuevoNodo(double nuevo_dato) {
 		NodoPila nuevo_nodo = new NodoPila(nuevo_dato, arriba);
 		arriba = nuevo_nodo;
+		
+		
+		
 	}
-	public double popPila( ) {
+	public double DatosPila( ) {
 		double dato_arriba = arriba.dato;
 		arriba = arriba.abajo;
 		return dato_arriba;
@@ -15,9 +24,15 @@ public class RPN {
 		this.commando = commando;
 	}
 	public double resultado( ) {
+		//variables generales
 		double a, b;
 		int j;
+		
+		b = DatosPila( );
+		a = DatosPila( );
+		
 		for(int i = 0; i < commando.length( ); i++) {
+			
 			// si es un digito
 			if(Character.isDigit(commando.charAt(i))) {
 				double numero;
@@ -30,42 +45,31 @@ public class RPN {
 				}
 				// convertir a double y añadir a la pila
 				numero = Double.parseDouble(temp);
-				pushPila(numero);
+				NuevoNodo(numero);
+				
+				//Dejamos solo las operaciones y las variables fuera
 			} else if(commando.charAt(i) == '+') {
-				b = popPila( );
-				a = popPila( );
-				pushPila(a + b);
+				NuevoNodo(a + b);
 			} else if(commando.charAt(i) == '-') {
-				b = popPila( );
-				a = popPila( );
-				pushPila(a - b);
+				NuevoNodo(a - b);
 			} else if(commando.charAt(i) == '*') {
-				b = popPila( );
-				a = popPila( );
-				pushPila(a * b);
+				NuevoNodo(a * b);
 			} else if(commando.charAt(i) == '/') {
-				b = popPila( );
-				a = popPila( );
-				pushPila(a / b);
+				NuevoNodo(a / b);
 			}
 			else if(commando.charAt(i) == '^') {
-				b = popPila( );
-				a = popPila( );
-				pushPila(Math.pow(a, b));}
+				NuevoNodo(Math.pow(a, b));}
 			else if(commando.charAt(i) == '%') {
-				b = popPila( );
-				a = popPila( );
-				pushPila(a%b);
+				NuevoNodo(a%b);
 			} else if(commando.charAt(i) != ' ') {
 				throw new IllegalArgumentException( );
 			}
 		}
-		double val = popPila( );
+		double val = DatosPila( );
 		if(arriba != null) {
 			throw new IllegalArgumentException( );
 		}
 		return val;
 	}
-	private String commando;
-	private NodoPila arriba;
+
 }
